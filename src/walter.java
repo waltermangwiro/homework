132f5b03020fa3d169bef50001ba541f5ae1612d
walter <wmangwi1@stu.vistula.edu.pl>
2025-02-28 16:34:53 +0100

    yv8

--- src/walter.java
+++ src/walter.java
@@ -2,6 +2,6 @@ public class walter {
     public static void main (String[]args) {
         System.out.println("Hello Worldj");
         System.out.println("walter am the goat");
-        System.out.println("thank you vey much");
+        System.out.println("thank you vey muchf");
     }
 }
